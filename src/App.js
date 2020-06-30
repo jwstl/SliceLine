05af192278ce4a61dd9b9c14035360e7448b32af
Mustang Sky <hello@mustangsky.com>
2020-06-30 08:53:24 -0400

    Global Style using Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
+    <>
+    <h1>Sliceline</h1>
+    <GlobalStyle></GlobalStyle>
     <div>Hello SliceLine</div>
+    </>
   );
 }
 
